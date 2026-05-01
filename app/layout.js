ba18bda46728aa6da279d99e59eac51ff99a0cf2
MahvOS <mahvinaflahmulyana@gmail.com>
2026-05-01 14:13:17 +0700

    changes first

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,15 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'Sketsaa IDN',
-  description: 'Aplikasi pembantu hafalan Al-Quran modern untuk persiapan UAS Tahfidz.',
-}
+  title: "Sketsaa IDN",
+  description:
+    "Aplikasi pembantu hafalan Al-Quran modern untuk persiapan UAS Tahfidz.",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <body>{children}</body>
     </html>
-  )
+  );
 }
